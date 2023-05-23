815ba200c1729f8c08cc9bb0943da6ddcc9c2f59
Evgenia <semenutenkoevgenia@gmail.com>
2023-05-23 20:42:19 +0300

    add absolute import

---
+++ src/Conteiner/App/App.tsx
@@ -0,0 +1,10 @@
+import Header from 'Conteiner/Header/Header'
+
+const App = () => {
+    return (
+        <>
+        <Header />
+        </>
+    )
+}
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Title from './Title'
-import Content from './Content'
+import App from './Conteiner/App/App'
 
 // const title = React.createElement('h1', { id: 'title'}, 'hello react')
 
-const App = () => {
-    return (
-        <>
-            <Title title="App" />
-            <Title title="React" num={1} />
-            <Content text1={'Lorem'} text2={'lorem'} />
-        </>
-    )
-}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
