903d5c8b49a2f5ffadd4d5f9d899f828487d344d
Evgenia <semenutenkoevgenia@gmail.com>
2023-05-30 20:12:51 +0300

    add product list

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,12 @@
-
+import ProductList from "components/Products/ProductList"
 type Props = {}
 
 
 const Home = (props: Props) => {
   return (
-    <div>Home</div>
+    <>
+    <ProductList/>
+    </>
   )
 }
 
