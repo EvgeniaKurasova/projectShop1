cfec22d88c025057165b4b57386cc053234e2b55
Evgenia <semenutenkoevgenia@gmail.com>
2023-06-01 14:28:51 +0300

    add productsArray

---
+++ src/utils/productsArray.ts
@@ -0,0 +1,58 @@
+type Product = {
+    title: string
+    description: string
+    capacity: string
+    type: string
+    price: number
+}
+
+
+const productsArray: Product[] = [
+    {
+    title: 'iPhone 14 Pro',
+    description: 'This is iPhone 14 Pro',
+    type: 'phone',
+    capacity: '256',
+    price: 1500,
+    },
+
+    {
+    title: 'iPhone 13 Pro',
+    description: 'This is iPhone 13 Pro',
+    type: 'phone',
+    capacity: '256',
+    price: 1000,
+    },
+    {
+    title: 'iPhone 13',
+    description: 'This is iPhone 13',
+    type: 'phone',
+    capacity: '128',
+    price: 1000,
+    },
+    {
+    title: 'iPhone 14',
+    description: 'This is iPhone 14',
+    type: 'phone',
+    capacity: '128',
+    price: 1000,
+    },
+    {
+    title: 'iPhone 11Pro MAX',
+    description: 'This is iPhone 11Pro MAX',
+    type: 'phone',
+    capacity: '512',
+    price: 1200,
+    },
+    {
+    title: 'iPhone XS',
+    description: 'This is iPhone XS',
+    type: 'phone',
+    capacity: '256',
+    price: 500,
+    },
+
+
+]
+
+export default productsArray
